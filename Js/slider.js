cfaa9c160f4fd298187679c4b72716e2e487b6f5
Ksompati2 <72212452+Ksompati2@users.noreply.github.com>
2022-06-29 05:11:32 +0600

    change

--- Js/slider.js
+++ Js/slider.js
@@ -1,6 +1,6 @@
-// $(function () {
-//   $(document).scroll(function () {
-//     var $nav = $(".fixed-top");
-//     $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
-//   });
-// });
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".fixed-top");
+    $nav.toggleClass("scrolled", $(this).scrollTop() > $nav.height());
+  });
+});
