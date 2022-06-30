1d5e651a64b546b6df109ee1bc3f4c732ebca6ea
Ksompati2 <72212452+Ksompati2@users.noreply.github.com>
2022-06-30 08:40:50 +0600

    chnage

--- Js/slider.js
+++ Js/slider.js
@@ -1,3 +1,12 @@
+document.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  const navbarHeight = 100;
+
+  const distanceFromTop = Math.abs(document.body.getBoundingClientRect().top);
+
+  if (distanceFromTop >= navbarHeight) navbar.classList.add("fixed-top");
+  else navbar.classList.remove("fixed-top");
+});
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".fixed-top");
